e2610a0e3454c10ea2fdf15fc9b2a73e88ed84a1
agnecom <nagnecom@naver.com>
2018-02-10 10:07:15 +0900

    1

--- Hotfix.java
+++ Hotfix.java
@@ -1,7 +1,86 @@
-class Hotfix {
-    private String hotfix;
-
-    public String getHotfix() {
-        this.hotfix;
-    }
+function View(common) {
+    this.common = common;
 }
+
+View.prototype.addNewArticle = function (article, callback1 = ()=>{}, callback2 = ()=>{}, rowClickListener = ()=>{}) {
+    var tableBody = this.common.select("table > tbody");
+    if ( tableBody ) {
+        var newRow = tableBody.insertRow(0);
+        var newCell = newRow.insertCell(0);
+        newCell.innerText = article.number;
+        newCell = newRow.insertCell(1);
+        newCell.innerText = article.subject;
+        newCell = newRow.insertCell(2);
+        newCell.dataset.id = article.writerId;
+        newCell.innerText = article.writer;
+        newCell = newRow.insertCell(3);
+        newCell.innerText = article.read;
+
+
+            callback1();
+
+            callback2();
+
+
+
+            rowClickListener(newRow);
+
+    }
+};
+
+View.prototype.addReverseNewArticle = function(article, callback1 = ()=>{}, callback2 = ()=>{}, rowClickListener = ()=>{}) {
+    var tableBody = this.common.select("table > tbody");
+    if ( tableBody  ) {
+        var newRow = tableBody.insertRow(tableBody.rows.length);
+        var newCell = newRow.insertCell(0);
+        newCell.innerText = article.number;
+        newCell = newRow.insertCell(1);
+        newCell.innerText = article.subject;
+        newCell = newRow.insertCell(2);
+        newCell.dataset.id = article.writerId;
+        newCell.innerText = article.writer;
+        newCell = newRow.insertCell(3);
+        newCell.innerText = article.read;
+
+
+            callback1();
+
+            callback2();
+
+
+
+            rowClickListener(newRow);
+
+    }
+};
+
+View.prototype.removeDetailRows = function (tbody) {
+    for ( var index in tbody.rows ) {
+        if ( tbody.rows[index].className == "detail" ) {
+            tbody.deleteRow(index);
+        }
+    }
+};
+
+View.prototype.addDetailView = function(tbody, rowIndex, content) {
+    var detailRow = tbody.insertRow(rowIndex);
+    detailRow.className = "detail";
+    var detailCell = detailRow.insertCell(0);
+    detailCell.colSpan = 4;
+    detailCell.style.paddingLeft = "40px";
+    detailCell.innerHTML = content;
+};
+
+View.prototype.removeAllArticles = function () {
+  // table > tbody 검색 후
+  const tbody = this.common.select("table > tbody");
+  // table > tbody > tr 을 모두
+  });
+
+
+
+};
+
+
+window.app = window.app || {};
+window.app.View = View;
