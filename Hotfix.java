9155e0695e0066ebc2d20145d6ef87ef2d0d3a87
agnecom <nagnecom@naver.com>
2018-02-10 10:17:42 +0900

    1111

--- Hotfix.java
+++ Hotfix.java
@@ -1,6 +1,7 @@
 function View(common) {
     this.common = common;
 }
+<<<<<<< HEAD
 
 View.prototype.addNewArticle = function (article, callback1 = ()=>{}, callback2 = ()=>{}, rowClickListener = ()=>{}) {
     var tableBody = this.common.select("table > tbody");
@@ -62,6 +63,69 @@ View.prototype.removeDetailRows = function (tbody) {
     }
 };
 
+=======
+
+View.prototype.addNewArticle = function (article, callback1 = ()=>{}, callback2 = ()=>{}, rowClickListener = ()=>{}) {
+    var tableBody = this.common.select("table > tbody");
+    if ( tableBody ) {
+        var newRow = tableBody.insertRow(0);
+        var newCell = newRow.insertCell(0);
+        newCell.innerText = article.number;
+        newCell = newRow.insertCell(1);
+        newCell.innerText = article.subject;
+        newCell = newRow.insertCell(2);
+        newCell.dataset.id = article.writerId;
+        newCell.innerText = article.writer;
+        newCell = newRow.insertCell(3);
+        newCell.innerText = article.read;
+
+
+            callback1();
+
+            callback2();
+
+
+
+            rowClickListener(newRow);
+
+    }
+};
+
+View.prototype.addReverseNewArticle = function(article, callback1 = ()=>{}, callback2 = ()=>{}, rowClickListener = ()=>{}) {
+    var tableBody = this.common.select("table > tbody");
+    if ( tableBody  ) {
+        var newRow = tableBody.insertRow(tableBody.rows.length);
+        var newCell = newRow.insertCell(0);
+        newCell.innerText = article.number;
+        newCell = newRow.insertCell(1);
+        newCell.innerText = article.subject;
+        newCell = newRow.insertCell(2);
+        newCell.dataset.id = article.writerId;
+        newCell.innerText = article.writer;
+        newCell = newRow.insertCell(3);
+        newCell.innerText = article.read;
+
+
+            callback1();
+
+            callback2();
+
+
+
+            rowClickListener(newRow);
+
+    }
+};
+
+View.prototype.removeDetailRows = function (tbody) {
+    for ( var index in tbody.rows ) {
+        if ( tbody.rows[index].className == "detail" ) {
+            tbody.deleteRow(index);
+        }
+    }
+};
+
+>>>>>>> develop
 View.prototype.addDetailView = function(tbody, rowIndex, content) {
     var detailRow = tbody.insertRow(rowIndex);
     detailRow.className = "detail";
@@ -74,6 +138,7 @@ View.prototype.addDetailView = function(tbody, rowIndex, content) {
 View.prototype.removeAllArticles = function () {
   // table > tbody 검색 후
   const tbody = this.common.select("table > tbody");
+<<<<<<< HEAD
   // table > tbody > tr 을 모두 삭제
   const rows = this.common.selectAll("tr", tbody);
 
@@ -82,6 +147,12 @@ View.prototype.removeAllArticles = function () {
   });
 
 
+=======
+  // table > tbody > tr 을 모두
+  });
+
+sdfsdf
+>>>>>>> develop
 
 };
 
